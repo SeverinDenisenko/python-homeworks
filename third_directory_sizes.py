b26fffae090f439a7498ab22c91180d55b6c482f
severin-denisenko <devchonkavkorobchonke@gmail.com>
2022-11-06 01:58:11 +0300

    Add 3.7

---
+++ third_directory_sizes.py
@@ -0,0 +1,25 @@
+import argparse
+from pathlib import Path
+
+
+# 3.7
+def main():
+    parser = argparse.ArgumentParser(description='Find sizes if files in directory.')
+    parser.add_argument('directory', type=str, help='Path to directory')
+    args = parser.parse_args()
+
+    directory = Path(args.directory)
+    files = [f for f in directory.iterdir() if f.is_file()]
+    sizes = [f.stat().st_size for f in files]
+    # directory = args.directory
+    # files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+    # sizes = [os.path.getsize(f) for f in files]
+
+    out = sorted(zip(sizes, files), reverse=True)
+
+    for item in out:
+        print(f"{item[1]} \t {item[0]}")
+
+
+if __name__ == "__main__":
+    main()
